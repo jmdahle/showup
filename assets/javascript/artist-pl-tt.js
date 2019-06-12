ff2eccb7f709fff0742619fbe5c08c263e8b591c
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-06-12 07:00:56 -0500

    updated playlist and toptrack 'panels'

--- assets/javascript/artist-pl-tt.js
+++ assets/javascript/artist-pl-tt.js
@@ -14,6 +14,7 @@ if (responseSpotify.length > 0) { // there is only a "hash" if spotify login ret
     console.log("token", spotifyToken);
 }
 
+
 // spotify login
 $("#spotifyLogin").on("click", function () {
     // opens the spotify login and returns with a token
@@ -25,42 +26,7 @@ $("#spotifyLogin").on("click", function () {
     window.open(combinedUrl, "_self");
 })
 
-$("#btnPlaylists").on("click", function() {
-    getPlaylist();
-});
-
-function getPlaylist() {
-    // ajax call for playlists featuring this artist
-    $.ajax({
-        url: "https://api.spotify.com/v1/search?q="+ artistName.replace(" ","+") + "&type=playlist",
-        headers: {
-            "Authorization": "Bearer " + spotifyToken
-        },
-        success: function (r2) {
-            console.log(r2);
-            populatePlaylists(r2);
-        }
-    });
-}
-
-$("#btnTopTracks").on("click", function() {
-    getTopTracks();
-});
-
-function getTopTracks() {
-    // ajax call for top tracks using artist ID and access token
-    $.ajax({
-        url: "https://api.spotify.com/v1/artists/" + artistId + "/top-tracks?country=US",
-        headers: {
-            "Authorization": "Bearer " + spotifyToken
-        },
-        success: function (r3) {
-            console.log(r3);
-            populateTopTracks(r3);
-        }
-    });
-}
-
+// TEST - input for an artist name
 $("#artist-name").on("change", function(){
     artistName = $("#artist-name").val();
     console.log("artist name", artistName);
@@ -68,6 +34,7 @@ $("#artist-name").on("change", function(){
     console.log(artistName,artistId);
 });
 
+// TEST - function for retriving the artist ID
 function getArtistId() {
     var searchName = artistName.replace(" ","+");
     $.ajax({
@@ -85,36 +52,90 @@ function getArtistId() {
 
 }
 
+// trigger for playlists to be shown
+$("#btnPlaylists").on("click", function() {
+    $("#btnPlaylists").attr("class","btn btn-success");
+    $("#btnTopTracks").attr("class","btn btn-secondary");
+    getPlaylist();
+});
+
+// playlist search API call
+function getPlaylist() {
+    // ajax call for playlists featuring this artist
+    $.ajax({
+        url: "https://api.spotify.com/v1/search?q="+ artistName.replace(" ","+") + "&type=playlist",
+        headers: {
+            "Authorization": "Bearer " + spotifyToken
+        },
+        success: function (r2) {
+            console.log(r2);
+            populatePlaylists(r2);
+        }
+    });
+}
+
+// trigger for top tracks to be shown
+$("#btnTopTracks").on("click", function() {
+    $("#btnTopTracks").attr("class","btn btn-success");
+    $("#btnPlaylists").attr("class","btn btn-secondary");
+    getTopTracks();
+});
+
+// top tracks for artist API call
+function getTopTracks() {
+    // ajax call for top tracks using artist ID and access token
+    $.ajax({
+        url: "https://api.spotify.com/v1/artists/" + artistId + "/top-tracks?country=US",
+        headers: {
+            "Authorization": "Bearer " + spotifyToken
+        },
+        success: function (r3) {
+            console.log(r3);
+            populateTopTracks(r3);
+        }
+    });
+}
+
+// update the links with artist playlists returned from API call
 function populatePlaylists(r) {
-    $("#artist-toptracks").hide();
-    $("#artist-playlists").show();
-    $("#artist-playlists").empty();
+    $("#artist-spotify-links").empty();
     for (var i = 0; i < r.playlists.items.length; i++) {
         var plName = r.playlists.items[i].name;
         var plUrl = r.playlists.items[i].external_urls.spotify;
-        var spotdiv = $("<div>");
-        var spotlink = $("<a>");
-        spotlink.attr("href", plUrl);
-        spotlink.text(plName);
-        spotlink.attr("target","_blank");
-        spotdiv.append(spotlink);
-        $("#artist-playlists").append(spotdiv);
+        var plId = r.playlists.items[i].id;
+        var spotOpt = $("<button>");
+        spotOpt.attr("type","button");
+        spotOpt.attr("class","btn btn-dark playlistOption");
+        spotOpt.attr("url",plUrl);
+        spotOpt.attr("playlistId",plId);
+        spotOpt.text(plName);
+        $("#artist-spotify-links").append(spotOpt);
+        $(".playlistOption").on("click",function(){
+            trackId = $(this).attr("playlistId");
+            trackUrl = $(this).attr("url");
+            window.open(trackUrl, "spotifyWindow");
+        });
     }
 }
 
+// update the links with artist top tracks returned from API call
 function populateTopTracks(r) {
-    $("#artist-playlists").hide();
-    $("#artist-toptracks").show();
-    $("#artist-toptracks").empty();
+    $("#artist-spotify-links").empty();
     for (var i = 0; i < r.tracks.length; i++) {
         var trName = r.tracks[i].name;
         var trUrl = r.tracks[i].external_urls.spotify;
-        var spotdiv = $("<div>");
-        var spotlink = $("<a>");
-        spotlink.attr("href", trUrl);
-        spotlink.text(trName);
-        spotlink.attr("target","_blank");
-        spotdiv.append(spotlink);
-        $("#artist-toptracks").append(spotdiv);
+        var trId = r.tracks[i].id;
+        var spotOpt = $("<button>");
+        spotOpt.attr("type","button");
+        spotOpt.attr("class","btn btn-dark trackOption");
+        spotOpt.attr("url",trUrl);
+        spotOpt.attr("trackId",trId)
+        spotOpt.text(trName);
+        $("#artist-spotify-links").append(spotOpt);
+        $(".trackOption").on("click",function(){
+            trackId = $(this).attr("trackId");
+            trackUrl = $(this).attr("url");
+            window.open(trackUrl, "spotifyWindow");
+        });
     }
 }
